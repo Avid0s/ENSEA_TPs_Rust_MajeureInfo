c60210aae072f497f5ff1326ae87ca756d9d2a35
Avid0s <gabriel.vidon@ensea.fr>
2026-03-27 13:47:01 +0100

    changement archi fichiers

--- TP2/v1/Tp_logo/target/debug/Tp_logo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\gjvid\Documents\GitHub\ENSEA_TPs_Rust_MajeureInfo\TP2\v1\Tp_logo\target\debug\Tp_logo.exe: C:\Users\gjvid\Documents\GitHub\ENSEA_TPs_Rust_MajeureInfo\TP2\v1\Tp_logo\src\main.rs
--- TP2/v1/Tp_logo/target/debug/deps/Tp_logo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\gjvid\Documents\GitHub\ENSEA_TPs_Rust_MajeureInfo\TP2\v1\Tp_logo\target\debug\deps\Tp_logo.d: src\main.rs
-
-C:\Users\gjvid\Documents\GitHub\ENSEA_TPs_Rust_MajeureInfo\TP2\v1\Tp_logo\target\debug\deps\Tp_logo.exe: src\main.rs
-
-src\main.rs:
